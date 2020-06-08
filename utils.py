ec99bb15395c1f803a1e5e5e42b130ad496c85de
Jose Tlacuilo <A014020128@itesm.mx>
2020-06-08 18:09:59 -0500

    fix empty values

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ def gage(_birth):
     today = date.today()
     return today.year - _birth.year - \
             ((today.month, today.day) < (_birth.month, _birth.day))
+
+def fempties(list):
+    for item in list:
+        if not item:
+            return True
+    return False
